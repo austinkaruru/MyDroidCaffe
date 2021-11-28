a6659c0ec22592a88587692eb51c768349142696
austinkaruru <karuruaustine@gmail.com>
2021-11-28 14:31:10 +0300

    First changes

--- app/src/main/java/com/example/mydroidcaffe/MainActivity.java
+++ app/src/main/java/com/example/mydroidcaffe/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.mydroidcaffe;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.mydroidcaffe.PagerAdapter;
 import com.example.mydroidcaffe.R;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
         //inflate toolbar
         Toolbar toolbar = findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         TabLayout tablayout = findViewById(R.id.tab_layout);
         tablayout.addTab(tablayout.newTab().setText(R.string.tab_label_1));
@@ -50,5 +65,46 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_pizza:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Pizza()).commit();
+
+                break;
+            case R.id.nav_cocktails:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Cocktail()).commit();
+
+                break;
+            case R.id.nav_pasta:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Pasta()).commit();
+
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_aboutus:
+                Toast.makeText(this, "About Us", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_contact:
+                Toast.makeText(this, "Contact Us", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
